fc0690947af23ff00f15126297bcc9ec5cbebfe1
Raineetu <raineetu0070@gmail.com>
2024-11-14 18:52:53 +0545

    Updated

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import fs from 'fs';
 import path from 'path';
-import animeDetailsRoute from './routes/animeDetailsRoute.js';
+import animeDetailsroute from './routes/animeDetailsRoute.js';
 import userRoute from './routes/userRoute.js';
 import commentRoutes from './routes/commentRoutes.js';
 import recommendationRoutes from './routes/recommendationRoute.js'; 
